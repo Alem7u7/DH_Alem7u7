3e8d0d994ebe04be1da2fb0092aec2eed0c5b480
Alem7u7 <soga981014g70@outlook.com>
2023-08-10 23:47:00 -0600

    Mejorando agregaCaminos

--- programacionImperativa/C7S/agregaCaminos.js
+++ programacionImperativa/C7S/agregaCaminos.js
@@ -4,7 +4,7 @@ let edad;
 let negativo = -1;
 
 while (negativo == -1) {
-    edad = parseInt(prompt("Ingrese su edad:"));
+    edad = parseInt(prompt("Ingrese su edad: "));
     negativo = (Math.sign(edad));
 
     if (negativo == -1) {
@@ -12,12 +12,26 @@ while (negativo == -1) {
     }
 }
 
+function edadImpar(edad) {
+    let impar = edad%2;
+    if(impar == 1){ return "Sabias que tu edad es impar?"} else {return ""} 
+}
+
 if(edad < 18) {
     console.log("No puede pasar al bar.");
+    console.log(edadImpar(edad));
 } else if (edad < 21){
     console.log("Puede pasar al bar, pero no puede tomar alcohol.");
+    console.log(edadImpar(edad));
 } else if (edad == 21){
     console.log("Puede pasar al bar, felicidades por haber llegado a la mayoría de edad.");
+    console.log(edadImpar(edad));
 } else {
     console.log("Puede pasar al bar y tomar alcohol.")
+    console.log(edadImpar(edad));
 }
+
+
+
+
+
