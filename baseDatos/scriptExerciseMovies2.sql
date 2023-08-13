632504c534d8fde725ddd7fbef3cb78b8e8fe7ce
Alem7u7 <soga981014g70@outlook.com>
2023-08-13 16:53:40 -0600

    Creando carpeta baseDatos y subiendo ejercicios

---
+++ baseDatos/scriptExerciseMovies2.sql
@@ -0,0 +1,7 @@
+/*select title as 'titulo' from movies_db.movies as peliculas;*/
+
+/*select title, rating, awards, release_date from movies_db.movies where rating > 3 and awards > 1 and release_date between '1988-01-01' and '2009-12-31'order by rating limit 3 offset 10; */
+
+/*select title, rating, awards, release_date from movies_db.movies where rating > 3 and awards > 1 and release_date between '1988-01-01' and '2009-12-31'order by rating asc limit 3;*/
+
+select * from movies_db.actors;
